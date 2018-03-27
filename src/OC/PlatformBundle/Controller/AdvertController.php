1cdf2c6303b6b06a1afa7c830c6cfae59be28bd6
mgaidiwahid <mgaidi.wahid@gmail.com>
2018-03-27 17:57:16 +0100

    create route and template

---
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace OC\PlatformBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class AdvertController extends Controller
+{
+    public function indexAction()
+    {
+        $content = $this->get('templating')
+                        ->render('OCPlatformBundle:Advert:index.html.twig', array('nom' => 'winzou'));
+        return new Response($content);
+    }
+}
