52debdc7a9ebdeba6de1f1a8f397f6b92e6dca7e
mgaidiwahid <mgaidi.wahid@gmail.com>
2018-03-28 16:49:11 +0100

    ajout des routes

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -4,19 +4,28 @@ namespace OC\PlatformBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class AdvertController extends Controller
 {
     public function indexAction()
     {
-        $content = $this->get('templating')
-                        ->render('OCPlatformBundle:Advert:index.html.twig', array('nom' => 'winzou'));
-        return new Response($content);
+        $url = $this->get('router')->generate(
+            'oc_platform_view', // 1er argument : le nom de la route
+            array('id' => 5),    // 2e argument : les valeurs des paramètres
+            UrlGeneratorInterface::ABSOLUTE_URL
+        );
+        return new Response("L'URL de l'annonce d'id 5 est : ".$url);
     }
-    public function quitAction()
+    public function viewAction($id)
     {
-        $content = $this->get('templating')
-                        ->render('OCPlatformBundle:Advert:quit.html.twig', array('prenom' => 'admin'));
-        return new Response($content);
-    }    
+        return new Response("Affichage de l'annonce d'id : ".$id);
+    }   
+    public function viewSlugAction($slug, $year, $_format)
+    {
+        return new Response(
+            "On pourrait afficher l'annonce correspondant au
+            slug '".$slug."', créée en ".$year." et au format ".$_format."."
+        );
+    }      
 }
